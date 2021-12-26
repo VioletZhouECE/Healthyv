55d252df5f22b4c7c27fcbeac41b8a79836ab592
Xinyi Zhou <x375zhou@uwaterloo.ca>
2021-12-26 13:53:39 -0500

    Initial Commit

---
+++ first_ios_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first_ios_app
+//
+//  Created by violet on 2021-12-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first_ios_app/first_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_ios_appApp.swift
+//  first_ios_app
+//
+//  Created by violet on 2021-12-26.
+//
+
+import SwiftUI
+
+@main
+struct first_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
