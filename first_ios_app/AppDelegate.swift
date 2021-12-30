fb6f5c0f43da708d8598b124fbc58e7e9a49c76b
Xinyi Zhou <x375zhou@uwaterloo.ca>
2021-12-30 14:58:00 -0500

    show notification when app is running in the foreground

---
+++ first_ios_app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  first_ios_app
+//
+//  Created by violet on 2021-12-30.
+//
+
+import Foundation
+import SwiftUI
+
+// AppDelegate.swift
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Here we actually handle the notification
+        print("Notification received with identifier \(notification.request.identifier)")
+        // So we call the completionHandler telling that the notification should display a banner and play the notification sound - this will happen while the app is in foreground
+        completionHandler([.banner, .sound])
+    }
+}
--- first_ios_app/first_ios_appApp.swift
+++ first_ios_app/first_ios_appApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct first_ios_appApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     init(){
         setNotifications()
         registerTimeIntervalNotif()
